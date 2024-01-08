adfa2115dd26f41d0b61ee0ef4864f011d847943
harivishvanath7 <harivishvanath7@gmail.com>
2024-01-09 00:10:53 +0530

    7th commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './Inline';
-import './appStyles.css';
-import styles from './appStyles.module.css';
+import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className='error'>Error</h1>
-      <h1 className={styles.success}>Success</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Form/>
+      </div>
+    )
+  }
 }
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
+export default App
